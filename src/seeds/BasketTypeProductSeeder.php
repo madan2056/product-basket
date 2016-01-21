16468a9604b5c9d1b9d3df38ba16b954f13b5d7f
Madan Adhikari <madan2056@gmail.com>
2016-01-21 14:46:41 +0545

    Use DB class

--- src/seeds/BasketTypeProductSeeder.php
+++ src/seeds/BasketTypeProductSeeder.php
@@ -2,13 +2,16 @@
 namespace Neptrox\ProductBasket;
 
 use Illuminate\Database\Seeder;
+use DB;
 
 class BasketTypeProductSeeder extends Seeder
 {
     public function run()
     {
-        if (DB::table('{{ $table }}')->where('url', 'product-basket')->count() == 0) {
-            DB::table('{{ $table }}')->insert([
+        $table = config(config('neptrox.product-basket-config').'.product-types-table');
+
+        if (DB::table($table)->where('url', 'product-basket')->count() == 0) {
+            DB::table($table)->insert([
                 'name' => 'Product Basket',
                 'url' => 'product-basket',
                 'status' => 1,
